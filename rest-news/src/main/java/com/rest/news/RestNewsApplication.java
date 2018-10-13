1856c19d3d26cfdb1fd783d46d2bd0cac41d5cda
“marcinp55 <marcinp55@gmail.com>
2018-10-13 14:22:14 +0200

    Initial commit

---
+++ rest-news/src/main/java/com/rest/news/RestNewsApplication.java
@@ -0,0 +1,12 @@
+package com.rest.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestNewsApplication.class, args);
+	}
+}
