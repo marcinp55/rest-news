dc6c414488c026477a68bb14cc98dbfced8780c3
“marcinp55 <marcinp55@gmail.com>
2018-10-15 00:33:12 +0200

    Front single article test

--- rest-news/bin/main/WebContent/script.js
+++ rest-news/bin/main/WebContent/script.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
 	$('#my_custom_button').click(function() {
 		$.ajax({
-			url: "http://localhost:8080/news/us/business"
+			url: "http://localhost:8080/news/us/technology"
 		}).then(function(data) {
-			$('.my_custom_div').empty();
-			$('.my_custom_div').append(data.country);
-			$('.my_custom_div').append(data.category);
-			$('.my_custom_div').append(data.topHeadlineDto.articles[0].title);
+			$('#country_p').html(data.country);
+			$('#category_p').html(data.category);
+			$('#article_title').html(data.topHeadlineDto.articles[0].title);
+			$('#article_img').attr('src', data.topHeadlineDto.articles[0].urlToImage);
+			$('#article_desc').html(data.topHeadlineDto.articles[0].description);
+			$('#article_auth').html(data.topHeadlineDto.articles[0].author);
+			$('#article_link').attr('href', data.topHeadlineDto.articles[0].url);
+			$('#article_date').html(data.topHeadlineDto.articles[0].publishedAt);
+			$('#article_src').html(data.topHeadlineDto.articles[0].source.name);
 		});
 	});
 });
