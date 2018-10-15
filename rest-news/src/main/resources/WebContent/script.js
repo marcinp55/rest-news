7bfc4d91a6b4b8c3a43153e016860c88b31394ba
“marcinp55 <marcinp55@gmail.com>
2018-10-15 13:12:17 +0200

    Styles

--- rest-news/src/main/resources/WebContent/script.js
+++ rest-news/src/main/resources/WebContent/script.js
@@ -2,47 +2,61 @@ $(document).ready(function() {
 	var datatableRowTemplate = $('[data-article-template]').children()[0];
 	var articlesContainer = $('[data-articles-container]');
 	
-	$('#my_custom_button').click(function() {
+	$('#show_articles_button').click(function() {
 		$.ajax({
-			url: "http://localhost:8080/news/us/technology"
+			url: "http://localhost:8080/news/pl/technology"
 		}).then(function(data) {
 			articlesContainer.empty();
+			$('[data-articles-country]').html(data.country);
+			$('[data-articles-category]').html(data.category);
 			
-			$.each(data.topHeadlineDto.articles, function(entryIndex, entry) {
+			$.each(data.topHeadlineDto.articles, function() {
 				var element = $(datatableRowTemplate).clone();
 				
-				element.find('[data-article-title]').html(this.title);
-				element.find('[data-article-img]').attr('src', this.urlToImage);
-				element.find('[data-article-desc]').html(this.description);
-				element.find('[data-article-auth]').html(this.author);
-				element.find('[data-article-link]').attr('href', this.url);
-				element.find('[data-article-date]').html(this.publishedAt);
-				element.find('[data-article-src]').html(this.source.name);
+				if(this.title != null) {
+					element.find('[data-article-title]').html(this.title);
+				} else {
+					element.find('[data-article-title]').html("No title provided.")
+				}
+				
+				if(this.urlToImage != null) {
+					element.find('[data-article-img]').attr('src', this.urlToImage);	
+				} else {
+					element.find('[data-article-img]').attr('alt', "Sorry. No image available.");
+				}
+				
+				if(this.description != null) {
+					element.find('[data-article-desc]').html(this.description);	
+				} else {
+					element.find('[data-article-desc]').html("No description provided.");
+				}
+				
+				if(this.author != null) {
+					element.find('[data-article-auth]').html(this.author);	
+				} else {
+					element.find('[data-article-auth]').html("No author provided.");
+				}
+				
+				if(this.url != null) {
+					element.find('[data-article-link]').attr('href', this.url);
+				} else {
+					element.find('[data-article-link]').html("No link provided.");
+				}
+				
+				if(this.publishedAt != null) {
+					element.find('[data-article-date]').html(this.publishedAt);	
+				} else {
+					element.find('[data-article-date]').html("No article date provided.");
+				}
+				
+				if(this.source.name != null) {
+					element.find('[data-article-src]').html(this.source.name);
+				} else {
+					element.find('[data-article-src]').html("No source provided.");
+				}
+				
 				element.appendTo(articlesContainer);
 			});
 		});
 	});
-	
-	function createElement(data) {
-		var element = $(datatableRowTemplate).clone();
-		
-		console.log(element);
-		
-		element.find('[data-article-title]').html(data.title);
-		
-		return element;
-	}
 });
-
-
-/*
-			$('#country_p').html(data.country);
-			$('#category_p').html(data.category);
-			$('#article_title').html(data.topHeadlineDto.articles[0].title);
-			$('#article_img').attr('src', data.topHeadlineDto.articles[0].urlToImage);
-			$('#article_desc').html(data.topHeadlineDto.articles[0].description);
-			$('#article_auth').html(data.topHeadlineDto.articles[0].author);
-			$('#article_link').attr('href', data.topHeadlineDto.articles[0].url);
-			$('#article_date').html(data.topHeadlineDto.articles[0].publishedAt);
-			$('#article_src').html(data.topHeadlineDto.articles[0].source.name);
-*/
