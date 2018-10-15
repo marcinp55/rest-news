658dce6991289724afefb8220bdc31174494b50f
“marcinp55 <marcinp55@gmail.com>
2018-10-15 11:45:29 +0200

    Multiple article dynamic display

--- rest-news/src/main/resources/WebContent/script.js
+++ rest-news/src/main/resources/WebContent/script.js
@@ -1,8 +1,41 @@
 $(document).ready(function() {
+	var datatableRowTemplate = $('[data-article-template]').children()[0];
+	var articlesContainer = $('[data-articles-container]');
+	
 	$('#my_custom_button').click(function() {
 		$.ajax({
 			url: "http://localhost:8080/news/us/technology"
 		}).then(function(data) {
+			articlesContainer.empty();
+			
+			$.each(data.topHeadlineDto.articles, function(entryIndex, entry) {
+				var element = $(datatableRowTemplate).clone();
+				
+				element.find('[data-article-title]').html(this.title);
+				element.find('[data-article-img]').attr('src', this.urlToImage);
+				element.find('[data-article-desc]').html(this.description);
+				element.find('[data-article-auth]').html(this.author);
+				element.find('[data-article-link]').attr('href', this.url);
+				element.find('[data-article-date]').html(this.publishedAt);
+				element.find('[data-article-src]').html(this.source.name);
+				element.appendTo(articlesContainer);
+			});
+		});
+	});
+	
+	function createElement(data) {
+		var element = $(datatableRowTemplate).clone();
+		
+		console.log(element);
+		
+		element.find('[data-article-title]').html(data.title);
+		
+		return element;
+	}
+});
+
+
+/*
 			$('#country_p').html(data.country);
 			$('#category_p').html(data.category);
 			$('#article_title').html(data.topHeadlineDto.articles[0].title);
@@ -12,6 +45,4 @@ $(document).ready(function() {
 			$('#article_link').attr('href', data.topHeadlineDto.articles[0].url);
 			$('#article_date').html(data.topHeadlineDto.articles[0].publishedAt);
 			$('#article_src').html(data.topHeadlineDto.articles[0].source.name);
-		});
-	});
-});
+*/
